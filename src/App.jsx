ea077005aec8dc02614a3d418d8d226804ccaee2
hua <a1113791@gmail.com>
2025-01-15 01:34:26 +0800

    更

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,367 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import { Modal } from 'bootstrap';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const API_PATH = import.meta.env.VITE_API_PATH;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isAuth, steIsAuth] = useState(false);
+
+  const [account, setAccount] = useState({
+    username: '',
+    password: '',
+  });
+
+  const [tempProduct, setTempProduct] = useState({});
+  const [products, setProducts] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { value, name } = e.target;
+    setAccount({ ...account, [name]: value });
+  };
+
+  const getProducts = () => {
+    axios
+      .get(`${BASE_URL}/v2/api/${API_PATH}/admin/products`)
+      .then((res) => setProducts(res.data.products))
+      .catch(() => alert('取得產品失敗'));
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${BASE_URL}/v2/admin/signin`, account)
+      .then((res) => {
+        const { token, expired } = res.data;
+        document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+        axios.defaults.headers.common['Authorization'] = token;
+        axios
+          .get(`${BASE_URL}/v2/api/${API_PATH}/admin/products`)
+          .then((res) => setProducts(res.data.products))
+          .catch((error) => console.error(error));
+        steIsAuth(true);
+      })
+      .catch(() => alert('登入失敗'));
+  };
+
+  const checkUserLogin = () => {
+    axios
+      .post(`${BASE_URL}/v2/api/user/check`)
+      .then(() => {
+        getProducts();
+        steIsAuth(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
+      '$1'
+    );
+    axios.defaults.headers.common['Authorization'] = token;
+    checkUserLogin();
+  }, []);
+
+  const productModalRef = useRef(null);
+
+  useEffect(() => {
+    new Modal(productModalRef.current);
+  }, []);
+
+  const handleOpenProductModal = () => {
+    const modalInstance = Modal.getInstance(productModalRef.current);
+    modalInstance.show();
+  };
+  const handleCloseProductModal = () => {
+    const modalInstance = Modal.getInstance(productModalRef.current);
+    modalInstance.hide();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {isAuth ? (
+        <div className="container py-5">
+          <div className="row">
+            <div className="col">
+              <div className="d-flex justify-content-between">
+                <h2>產品列表</h2>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={handleOpenProductModal}
+                >
+                  建立新的產品
+                </button>
+              </div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">產品名稱</th>
+                    <th scope="col">原價</th>
+                    <th scope="col">售價</th>
+                    <th scope="col">是否啟用</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map((product) => (
+                    <tr key={product.id}>
+                      <th scope="row">{product.title}</th>
+                      <td>{product.origin_price}</td>
+                      <td>{product.price}</td>
+                      <td>{product.is_enabled}</td>
+                      <td>
+                        <div className="btn-group">
+                          <button
+                            type="button"
+                            className="btn btn-outline-primary btn-sm"
+                            onClick={handleOpenProductModal}
+                          >
+                            編輯
+                          </button>
+                          <button
+                            type="button"
+                            className="btn btn-outline-danger btn-sm"
+                          >
+                            刪除
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+          <h1 className="mb-5">請先登入</h1>
+          <form className="d-flex flex-column gap-3" onSubmit={handleLogin}>
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="username"
+                name="username"
+                placeholder="name@example.com"
+                value={account.username}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="username">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={account.password}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+            <button className="btn btn-primary">登入</button>
+          </form>
+          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
+        </div>
+      )}
+
+      <div
+        id="productModal"
+        className="modal"
+        ref={productModalRef}
+        style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}
+      >
+        <div className="modal-dialog modal-dialog-centered modal-xl">
+          <div className="modal-content border-0 shadow">
+            <div className="modal-header border-bottom">
+              <h5 className="modal-title fs-4">新增產品</h5>
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={handleCloseProductModal}
+              ></button>
+            </div>
+
+            <div className="modal-body p-4">
+              <div className="row g-4">
+                <div className="col-md-4">
+                  <div className="mb-4">
+                    <label htmlFor="primary-image" className="form-label">
+                      主圖
+                    </label>
+                    <div className="input-group">
+                      <input
+                        name="imageUrl"
+                        type="text"
+                        id="primary-image"
+                        className="form-control"
+                        placeholder="請輸入圖片連結"
+                      />
+                    </div>
+                    <img src="" alt="" className="img-fluid" />
+                  </div>
+
+                  {/* 副圖 */}
+                  <div className="border border-2 border-dashed rounded-3 p-3">
+                    {tempProduct.imagesUrl?.map((image, index) => (
+                      <div key={index} className="mb-2">
+                        <label
+                          htmlFor={`imagesUrl-${index + 1}`}
+                          className="form-label"
+                        >
+                          副圖 {index + 1}
+                        </label>
+                        <input
+                          id={`imagesUrl-${index + 1}`}
+                          type="text"
+                          placeholder={`圖片網址 ${index + 1}`}
+                          className="form-control mb-2"
+                        />
+                        {image && (
+                          <img
+                            src={image}
+                            alt={`副圖 ${index + 1}`}
+                            className="img-fluid mb-2"
+                          />
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+
+                <div className="col-md-8">
+                  <div className="mb-3">
+                    <label htmlFor="title" className="form-label">
+                      標題
+                    </label>
+                    <input
+                      name="title"
+                      id="title"
+                      type="text"
+                      className="form-control"
+                      placeholder="請輸入標題"
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label htmlFor="category" className="form-label">
+                      分類
+                    </label>
+                    <input
+                      name="category"
+                      id="category"
+                      type="text"
+                      className="form-control"
+                      placeholder="請輸入分類"
+                    />
+                  </div>
+
+                  <div className="mb-3">
+                    <label htmlFor="unit" className="form-label">
+                      單位
+                    </label>
+                    <input
+                      name="unit"
+                      id="unit"
+                      type="text"
+                      className="form-control"
+                      placeholder="請輸入單位"
+                    />
+                  </div>
+
+                  <div className="row g-3 mb-3">
+                    <div className="col-6">
+                      <label htmlFor="origin_price" className="form-label">
+                        原價
+                      </label>
+                      <input
+                        name="origin_price"
+                        id="origin_price"
+                        type="number"
+                        className="form-control"
+                        placeholder="請輸入原價"
+                      />
+                    </div>
+                    <div className="col-6">
+                      <label htmlFor="price" className="form-label">
+                        售價
+                      </label>
+                      <input
+                        name="price"
+                        id="price"
+                        type="number"
+                        className="form-control"
+                        placeholder="請輸入售價"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="mb-3">
+                    <label htmlFor="description" className="form-label">
+                      產品描述
+                    </label>
+                    <textarea
+                      name="description"
+                      id="description"
+                      className="form-control"
+                      rows={4}
+                      placeholder="請輸入產品描述"
+                    ></textarea>
+                  </div>
+
+                  <div className="mb-3">
+                    <label htmlFor="content" className="form-label">
+                      說明內容
+                    </label>
+                    <textarea
+                      name="content"
+                      id="content"
+                      className="form-control"
+                      rows={4}
+                      placeholder="請輸入說明內容"
+                    ></textarea>
+                  </div>
+
+                  <div className="form-check">
+                    <input
+                      name="is_enabled"
+                      type="checkbox"
+                      className="form-check-input"
+                      id="isEnabled"
+                    />
+                    <label className="form-check-label" htmlFor="isEnabled">
+                      是否啟用
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="modal-footer border-top bg-light">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={handleCloseProductModal}
+              >
+                取消
+              </button>
+              <button type="button" className="btn btn-primary">
+                確認
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
